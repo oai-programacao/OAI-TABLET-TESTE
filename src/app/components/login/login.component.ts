7f2808cebb1672f76edc951baa7d0b7cb7f70edb
Diego Soares <dsadamovich@gmail.com>
2025-09-03 15:18:41 -0300

    finishing window of login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,28 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { IftaLabelModule } from 'primeng/iftalabel';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { PasswordModule } from 'primeng/password';
+import { FloatLabelModule } from 'primeng/floatlabel';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    CardModule,
+    CommonModule,
+    InputTextModule,
+    ButtonModule,
+    IftaLabelModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    PasswordModule,
+    FloatLabelModule
+  ],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent {
-
-}
+export class LoginComponent {}
