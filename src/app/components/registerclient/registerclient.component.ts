66c872f8d732248652fb96be418b7aea7c11678a
Diego Soares <dsadamovich@gmail.com>
2025-09-08 12:39:31 -0300

    View Register Client

--- src/app/components/registerclient/registerclient.component.ts
+++ src/app/components/registerclient/registerclient.component.ts
@@ -1,27 +1,50 @@
 import { Component } from '@angular/core';
 import { CardBaseComponent } from '../../shared/components/card-base/card-base.component';
-import { InputText, InputTextModule } from 'primeng/inputtext';
+import { InputTextModule } from 'primeng/inputtext';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { IftaLabelModule } from 'primeng/iftalabel';
 import { InputGroupModule } from 'primeng/inputgroup';
 import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
 import { DividerModule } from 'primeng/divider';
+import { DatePickerModule } from 'primeng/datepicker';
+import { FormsModule } from '@angular/forms';
+import { SelectModule } from 'primeng/select';
+import { RadioButtonModule } from 'primeng/radiobutton';
 
 @Component({
   selector: 'app-registerclient',
   imports: [
     CardBaseComponent,
-    InputText,
     CommonModule,
     InputTextModule,
     ButtonModule,
     IftaLabelModule,
     InputGroupModule,
     InputGroupAddonModule,
-    DividerModule
+    DividerModule,
+    DatePickerModule,
+    FormsModule,
+    SelectModule,
+
   ],
   templateUrl: './registerclient.component.html',
   styleUrl: './registerclient.component.scss',
 })
-export class RegisterclientComponent {}
+export class RegisterclientComponent {
+  dateOfBirth: Date | null = null;
+  selectedOption: boolean = false;
+  clientLocation: string | null = null;
+
+
+  selectedClientType: { label: string; value: boolean } | null = null;
+
+  clientTypes = [
+    { label: 'Pessoa Física', value: true },
+    { label: 'Pessoa Jurídica', value: false },
+  ];
+
+  
+
+  
+}
