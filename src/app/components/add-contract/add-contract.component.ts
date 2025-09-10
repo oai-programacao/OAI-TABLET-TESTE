af61461acc9c60878253074a1eefe2e03244918f
Gabriel Santos <gabrielmath14@outlook.com>
2025-09-10 09:17:25 -0300

    add view

--- src/app/components/add-contract/add-contract.component.ts
+++ src/app/components/add-contract/add-contract.component.ts
@@ -2,10 +2,28 @@ import { Component } from '@angular/core';
 import { CardBaseComponent } from '../../shared/components/card-base/card-base.component';
 import { StepperModule } from 'primeng/stepper';
 import { ButtonModule } from 'primeng/button';
+import { FormsModule } from '@angular/forms';
+import { NgxMaskDirective } from 'ngx-mask';
+import { CommonModule } from '@angular/common';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { IftaLabelModule } from 'primeng/iftalabel';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-add-contract',
-  imports: [CardBaseComponent, StepperModule, ButtonModule],
+  imports: [
+    CardBaseComponent,
+    StepperModule,
+    ButtonModule,
+    FormsModule,
+    CommonModule,
+    NgxMaskDirective,
+    InputGroupModule,
+    IftaLabelModule,
+    InputGroupAddonModule,
+    InputTextModule
+  ],
   templateUrl: './add-contract.component.html',
   styleUrl: './add-contract.component.scss',
 })
