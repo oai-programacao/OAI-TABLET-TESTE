cfdc1384c1ece1546d9427075a9b583b544c42cb
unknown <gabrielmath14@outlook.com>
2025-09-10 09:16:19 -0300

    finalizando tela de info

--- src/app/components/info-client/info-client.component.ts
+++ src/app/components/info-client/info-client.component.ts
@@ -1,28 +1,104 @@
 // ANGULAR
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 //COMPONENTS
 import { CardBaseComponent } from "../../shared/components/card-base/card-base.component";
-
+import { MessagesValidFormsComponent } from '../../shared/components/message-valid-forms/message-valid-forms.component';
 
 // PRIMENG
 import { DividerModule } from 'primeng/divider';
 import { AvatarModule } from 'primeng/avatar';
-import { AvatarGroupModule } from 'primeng/avatargroup';
 import { InputTextModule } from 'primeng/inputtext';
-import { DatePickerModule } from 'primeng/datepicker';
 import { TextareaModule } from 'primeng/textarea';
 import { ButtonModule } from 'primeng/button';
-
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { IftaLabelModule } from 'primeng/iftalabel';
+import { CepService } from '../../services/cep.service';
 
 @Component({
   selector: 'app-info-client',
-  imports: [CommonModule, CardBaseComponent, DividerModule, AvatarModule, AvatarGroupModule, InputTextModule, DatePickerModule, FormsModule, TextareaModule, ButtonModule],
+  imports: [
+    CommonModule,
+    FormsModule,           
+    CardBaseComponent,
+    DividerModule,
+    InputTextModule,
+    TextareaModule,
+    ButtonModule,
+    AvatarModule,
+    MessagesValidFormsComponent,
+    FloatLabelModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    IftaLabelModule
+  ],
   templateUrl: './info-client.component.html',
-  styleUrl: './info-client.component.scss'
+  styleUrls: ['./info-client.component.scss']
 })
 export class InfoClientComponent {
-  birthday: Date = new Date(1980, 7, 12);
+  isEditing = false;
+
+  private readonly cepService = inject(CepService);
+
+
+  // PF ou PJ
+  tipoCliente: 'PF' | 'PJ' = 'PF';
+
+  cliente = {
+    // PF
+    nome: '',
+    cpf: '',
+    rg: '',
+    nascimento: '',
+
+    // PJ
+    razaoSocial: '',
+    nomeFantasia: '',
+    cnpj: '',
+    ie: '',
+
+    // Endereço e contato
+    cep: '',
+    logradouro: '',
+    numero: '',
+    complemento: '',
+    uf: '',
+    observacao: '',
+    celular1: '',
+    celular2: '',
+    telefone: '',
+    email: ''
+  };
+
+  toggleEditing() {
+    this.isEditing = !this.isEditing;
+  }
+
+  saveCliente() {
+    console.log('Cliente salvo:', this.cliente);
+    this.isEditing = false;
+  }
+
+  searchCEP(){
+    const cep = this.cliente.cep?.replace(/\D/g, '');
+    if(cep?.length === 8){
+      this.cepService.searchCEP(cep).subscribe({
+        next: (dados) => {
+          if(!dados.erro){
+            this.cliente.logradouro = dados.logradouro || '';
+            this.cliente.uf = dados.uf || '';
+            this.cliente.complemento = dados.complemento || '';
+          } else{
+            console.log('CEP não encontrado');
+          }
+          
+        },
+        error: (err) => console.log('Erro ao buscar CEP',err)
+      })
+    }
+  }
 }
