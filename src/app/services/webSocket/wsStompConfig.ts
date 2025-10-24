74d5cc923a07afd070ca3fc995f105a1e37d25d1
Gabriel Santos <gabrielmath14@outlook.com>
2025-10-24 14:04:49 -0300

    stop reconnect ws

--- src/app/services/webSocket/wsStompConfig.ts
+++ src/app/services/webSocket/wsStompConfig.ts
@@ -3,7 +3,7 @@ import SockJS from "sockjs-client";
 
 export const wsStompConfig = {
   webSocketFactory: () => new SockJS(`${environment.apiUrl}/ws-connect`),
-  reconnectDelay: 5000,
+  reconnectDelay: 0,        // desliga reconexão automática
   heartbeatIncoming: 0,
-  heartbeatOutgoing: 0,
+  heartbeatOutgoing: 20000,
 };
