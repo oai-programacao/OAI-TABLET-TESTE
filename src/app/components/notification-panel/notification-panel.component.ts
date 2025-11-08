6d679123eb354750bba8b958b8dcd52888d3a3bf
Gabriel Santos <gabrielmath14@outlook.com>
2025-11-08 14:27:36 -0300

    test in tablet

---
+++ src/app/components/notification-panel/notification-panel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CardBaseComponent } from '../../shared/components/card-base/card-base.component';
+
+@Component({
+  selector: 'app-notification-panel',
+  imports: [CardBaseComponent],
+  templateUrl: './notification-panel.component.html',
+  styleUrl: './notification-panel.component.scss'
+})
+export class NotificationPanelComponent {
+
+}
