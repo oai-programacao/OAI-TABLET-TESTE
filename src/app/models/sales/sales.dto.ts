f6c4d0245b7c1f472af7aab513a781ec3d1436f8
Gabriela <gabiviana.cunha@gmail.com>
2025-11-14 12:10:41 -0300

    feat: add model for sale

---
+++ src/app/models/sales/sales.dto.ts
@@ -0,0 +1,42 @@
+
+export interface ContractFormData {
+  salesId?: number;
+  sellerId: string;
+  clientId: string;
+  codePlan: number;
+  dateStart: string;
+  dateSignature: string;
+  dateOfAssignment?: string;
+  dateExpired: string;
+  adesion: number;
+  numberParcels: number;
+  parcels: Parcel[];
+  address: Address;
+  discount: number;
+  signature: string;
+  imagesOne?: string;
+  observation?: string;
+  situationDescription?: string;
+  discountFixed?: number;
+  vigencia?: number;
+  cicleFatId?: number;
+  cicleBillingDayBase?: number;
+  cicleBillingExpired?: number;
+}
+
+export interface Parcel {
+  description: string;
+  dueDate: string;
+  price: number;
+}
+
+export interface Address {
+  zipCode: string;
+  state: string;
+  city: string;
+  street: string;
+  number: string;
+  complement?: string;
+  neighborhood: string;
+  type?: string;
+}
