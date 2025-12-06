f7fa6de1aa2e7ec4ff36aa933b6c6ae207fc7c86
Gabriel Santos <gabrielmath14@outlook.com>
2025-12-06 10:51:41 -0300

    test responsividade

--- src/app/shared/components/card-leads/card-leads.component.ts
+++ src/app/shared/components/card-leads/card-leads.component.ts
@@ -1,14 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { ConfirmDialog } from 'primeng/confirmdialog';
 
 @Component({
   selector: 'app-card-leads',
-  imports: [CommonModule, ButtonModule],
+  imports: [CommonModule, ButtonModule, ConfirmDialog],
   templateUrl: './card-leads.component.html',
   styleUrl: './card-leads.component.scss',
+  providers: [ConfirmationService, MessageService],
+  standalone: true,
 })
 export class CardLeadsComponent implements OnInit {
+  constructor(private confirmationService: ConfirmationService) {}
+
   @Input() data: any;
   @Output() onConvert = new EventEmitter<void>();
   @Output() onDelete = new EventEmitter<void>();
@@ -22,6 +28,21 @@ export class CardLeadsComponent implements OnInit {
       const baseDate = new Date(this.data.archivedAt);
       this.expirationDate = new Date(baseDate.setDate(baseDate.getDate() + 7));
     }
+  }
+
+  confirmDelete() {
+    this.confirmationService.confirm({
+      message: 'Tem certeza que deseja excluir esta venda arquivada?',
+      header: 'Confirmar exclusão',
+      icon: 'pi pi-exclamation-triangle',
+      acceptLabel: '👍',
+      rejectLabel: '👎',
+      acceptButtonStyleClass: 'p-button-success',
+      rejectButtonStyleClass: 'p-button-danger',
+      accept: () => {
+        this.deleteSale();
+      },
+    });
   }
 
   convertSale(): void {
